67ea3c33fa87ad1ce4f68ce24a538d2e8f365a07
Administrator <2411939416@qq.com>
2023-10-31 12:52:40 +0800

    更改

---
+++ src/main/java/cc/sakurarain/antiinventory/AntiInventory.java
@@ -0,0 +1,17 @@
+package cc.sakurarain.antiinventory;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AntiInventory extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
