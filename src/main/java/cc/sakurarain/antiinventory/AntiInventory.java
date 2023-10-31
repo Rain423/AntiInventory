60b482c392a3a1abfbfd835358ee345a31314ef6
Administrator <2411939416@qq.com>
2023-10-31 13:01:08 +0800

    更改

--- src/main/java/cc/sakurarain/antiinventory/AntiInventory.java
+++ src/main/java/cc/sakurarain/antiinventory/AntiInventory.java
@@ -1,12 +1,17 @@
 package cc.sakurarain.antiinventory;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class AntiInventory extends JavaPlugin {
+public final class AntiInventory extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        this.getServer().getPluginManager().registerEvents(this,this);
 
     }
 
@@ -14,4 +19,12 @@ public final class AntiInventory extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    @EventHandler
+    public void onPlayerSwapHandItemsEvent(PlayerSwapHandItemsEvent event){
+
+    }
+
+    @EventHandler
+    public void onInventoryClickEvent(InventoryClickEvent event){}
 }
